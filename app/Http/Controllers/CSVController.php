697d86dcde3eec5dd1b3088b177a977a5f1c86ae
OscarT <oticona@comtech.com.bo>
2023-11-13 19:15:05 -0400

    final 7

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -138,6 +138,7 @@ class CSVController extends Controller
             return redirect()->back()->with('success', 'Archivo CSV importado correctamente');
         } catch (\Throwable $th) {
             DB::rollBack();
+            // dd($th->getMessage());
             return redirect()->back()->with('error', 'Error al importar el archivo CSV: ' . $th->getMessage());
         }
 
